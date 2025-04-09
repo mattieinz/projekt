bb1f37d932909fa2889cc64d53c00daca3d19201
sugoma01 <mattim2762@gmail.com>
2025-04-09 13:06:36 +0200

    dings

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
-// Start der Session
 session_start();
 ?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Meine Anwendung</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 2rem;
-        }
-    </style>
+    <title>Login check</title>
 </head>
 <body>
 
@@ -23,12 +16,6 @@ if (isset($_SESSION['login']) && isset($_SESSION['passwort'])) {
     include './interface/login/login.php';
 }
 ?>
-
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Seite geladen und bereit.");
-});
-</script>
 
 </body>
 </html>

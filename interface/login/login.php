c6df9ae8f693459fb3edec0d6f7432d14f969adb
sugoma01 <mattim2762@gmail.com>
2025-04-14 08:12:20 +0200

    gick gich ghp

--- interface/login/login.php
+++ interface/login/login.php
@@ -1,4 +1,23 @@
-<form>
-    <input type="text">
-    <input type="text">
-</form>
+<!DOCTYPE html> 
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./interface/login/css/style.css">
+</head>
+
+<body>
+
+    <form>
+        <h1>Login</h1>
+        <input type="text">
+        <input type="text">
+
+        <button>go</button>
+    </form>
+
+</body>
+
+</html> 
